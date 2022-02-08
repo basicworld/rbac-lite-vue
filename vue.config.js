b136a39c171ae4ddeb92e51a5bb49512140144b2
wlfei <basicworld@163.com>
2022-02-08 23:56:00 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -62,7 +62,16 @@ module.exports = {
   },
   chainWebpack(config) {
     // it can improve the speed of the first screen, it is recommended to turn on preload
-    // config.plugins.delete('preload')
+    // https://github.com/PanJiaChen/vue-element-admin/commit/73a9ab03797dcc6ad91e2150280401857957884f
+    config.plugin('preload').tap(() => [
+      {
+        rel: 'preload',
+        // to ignore runtime.js
+        // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+        fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+        include: 'initial'
+      }
+    ])
 
     // when there are many pages, it will cause too many meaningless requests
     config.plugins.delete('prefetch')
@@ -81,17 +90,6 @@ module.exports = {
       .loader('svg-sprite-loader')
       .options({
         symbolId: 'icon-[name]'
-      })
-      .end()
-
-    // set preserveWhitespace
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .loader('vue-loader')
-      .tap(options => {
-        options.compilerOptions.preserveWhitespace = true
-        return options
       })
       .end()
 
@@ -130,6 +128,7 @@ module.exports = {
                 }
               }
             })
+          // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
           config.optimization.runtimeChunk('single')
         }
       )
